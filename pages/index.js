0a2258402baa56ca90133fb008d836c283d6c54a
Ariane Vasques <vasquesariane@yahoo.com.br>
2022-02-09 22:13:55 -0300

    adiciona CSS dos componentes da home e crie estrutura de template da home

--- pages/index.js
+++ pages/index.js
@@ -2,22 +2,24 @@ import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <div className="infos">
-        <div className="avatar">
-          <img src='https://github.com/arianevasques.png' />
+    <div className={styles.homeInfos}>
+      <div className={styles.homeImage}></div>
+      <div className={styles.infos}>
+        <div className={styles.avatar}>
+          <img className={styles.imagemPerfil} src='https://github.com/arianevasques.png' />
         </div>
-        <h1 className="mainTitle">Ariane Vasques</h1>
-        <div className="description">
+        <h1 className={styles.mainTitle}>Ariane Vasques</h1>
+        <div className={styles.description}>
           <p>
-            Olá! Tudo bem!? Sou a Ariane :))
+            Olá! 
+            <br/>Tudo bem!? Sou a Ariane :))
           </p>
           <p>
             Formada em Publicidade e Propaganda, sempre atuei com o setor comercial.
-
-            Em 2021 iniciei meus estudos como Desenvolvedora de Softwares Front-End, decidida a migrar de area e mergulhar no mundo da tecnologia de cabeça. Dedico meio periodo dos meus dias (todos os dias) para os estudos, para assim conseguir atingir meu objetivo de conquistar meu espaço nesse ramo.
-
-            No outro periodo, atuo como Assistente Comercial da Tyar, empresa de tecnologia que desenvolve soluções corporativas para TI e Infraestrutura. Trabalhamos com customização de Workstation, Render Farm CPU ou GPU, Servidores, Storage e Media Server Watchout, de acordo com a necessidade de cada cliente.
+            <br/>
+            Em 2021 iniciei meus estudos como Desenvolvedora de Softwares Front-End, decidida a migrar de area e mergulhar no mundo da tecnologia de cabeça. Dedico meio período dos meus dias (todos os dias) para os estudos, para assim conseguir atingir meu objetivo de conquistar meu espaço nesse ramo.
+            <br/>
+            No outro período, atuo como Assistente Comercial da Tyar, empresa de tecnologia que desenvolve soluções corporativas para TI e Infraestrutura. Trabalhamos com customização de Workstation, Render Farm CPU ou GPU, Servidores, Storage e Media Server Watchout, de acordo com a necessidade de cada cliente.
           </p>
           <p>
             Adoro desafios e aprender coisas novas, procurando sempre absorver o máximo de minhas experiências!
@@ -26,7 +28,7 @@ export default function Home() {
             Seja Bem-vindx! :)
           </p>
         </div>
-        <div className="socialLinks">
+        <div className={styles.socialLinks}>
           <ul>
             <li>
               <a href="https://www.linkedin.com/in/ariane-vasques-a892a1183/">LinkedIn
@@ -43,8 +45,6 @@ export default function Home() {
           </ul>
         </div>
       </div>
-
-
     </div>
   )
 }
